b99edbe48635c02de8b9da84ab29441112fd7f16
kimh559 <kimh559@newschool.edu>
2022-03-08 08:08:06 -0500

    update

--- studio-2022/unit-2/javascript/index.js
+++ studio-2022/unit-2/javascript/index.js
@@ -1,14 +1,75 @@
-$(".js-open-modal").click(function(){
+$(".js-open-modal1").click(function(){
   $(".modal").addClass("visible");
 });
 
-$(".js-close-modal").click(function(){
+$(".js-close-modal1").click(function(){
   $(".modal").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".modal,.js-open-modal").length) {
+  if (!$(event.target).closest(".js-open-modal1").length) {
     $("body").find(".modal").removeClass("visible");
   }
 });
+
+$(".js-open-modal2").click(function(){
+  $(".modal").addClass("visible");
+});
+
+$(".js-close-modal2").click(function(){
+  $(".modal").removeClass("visible");
+});
+
+$(document).click(function(event) {
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest(".js-open-modal1").length) {
+    $("body").find(".modal").removeClass("visible");
+  }
+});
+
+$(".js-open-modal3").click(function(){
+  $(".modal").addClass("visible");
+});
+
+$(".js-close-modal3").click(function(){
+  $(".modal").removeClass("visible");
+});
+
+$(document).click(function(event) {
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest(".js-open-modal3").length) {
+    $("body").find(".modal").removeClass("visible");
+  }
+});
+
+$(".js-open-modal4").click(function(){
+  $(".modal").addClass("visible");
+});
+
+$(".js-close-modal4").click(function(){
+  $(".modal").removeClass("visible");
+});
+
+$(document).click(function(event) {
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest(".js-open-modal4").length) {
+    $("body").find(".modal").removeClass("visible");
+  }
+});
+
+$(".js-open-modal5").click(function(){
+  $(".modal").addClass("visible");
+});
+
+$(".js-close-modal5").click(function(){
+  $(".modal").removeClass("visible");
+});
+
+$(document).click(function(event) {
+  //if you click on anything except the modal itself or the "open modal" link, close the modal
+  if (!$(event.target).closest(".js-open-modal5").length) {
+    $("body").find(".modal").removeClass("visible");
+  }
+});
+
