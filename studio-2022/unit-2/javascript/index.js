5bedf9c98bcac0b2b72bb64a39cfdc355a1ce0fd
kimh559 <kimh559@newschool.edu>
2022-03-08 09:15:29 -0500

    update

--- studio-2022/unit-2/javascript/index.js
+++ studio-2022/unit-2/javascript/index.js
@@ -1,75 +1,75 @@
 $(".js-open-modal1").click(function(){
-  $(".modal").addClass("visible");
+  $(".modal1").addClass("visible");
 });
 
 $(".js-close-modal1").click(function(){
-  $(".modal").removeClass("visible");
+  $(".modal1").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".js-open-modal1").length) {
-    $("body").find(".modal").removeClass("visible");
+  if (!$(event.target).closest(".modal1, .js-open-modal1").length) {
+    $("body").find(".modal1").removeClass("visible");
   }
 });
 
 $(".js-open-modal2").click(function(){
-  $(".modal").addClass("visible");
+  $(".modal2").addClass("visible");
 });
 
 $(".js-close-modal2").click(function(){
-  $(".modal").removeClass("visible");
+  $(".modal2").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".js-open-modal1").length) {
-    $("body").find(".modal").removeClass("visible");
+  if (!$(event.target).closest(".modal2, .js-open-modal1").length) {
+    $("body").find(".modal2").removeClass("visible");
   }
 });
 
 $(".js-open-modal3").click(function(){
-  $(".modal").addClass("visible");
+  $(".modal3").addClass("visible");
 });
 
 $(".js-close-modal3").click(function(){
-  $(".modal").removeClass("visible");
+  $(".modal3").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".js-open-modal3").length) {
-    $("body").find(".modal").removeClass("visible");
+  if (!$(event.target).closest(".modal3, .js-open-modal3").length) {
+    $("body").find(".modal3").removeClass("visible");
   }
 });
 
 $(".js-open-modal4").click(function(){
-  $(".modal").addClass("visible");
+  $(".modal4").addClass("visible");
 });
 
 $(".js-close-modal4").click(function(){
-  $(".modal").removeClass("visible");
+  $(".modal4").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".js-open-modal4").length) {
-    $("body").find(".modal").removeClass("visible");
+  if (!$(event.target).closest(".modal4, .js-open-modal4").length) {
+    $("body").find(".modal4").removeClass("visible");
   }
 });
 
 $(".js-open-modal5").click(function(){
-  $(".modal").addClass("visible");
+  $(".modal5").addClass("visible");
 });
 
 $(".js-close-modal5").click(function(){
-  $(".modal").removeClass("visible");
+  $(".modal5").removeClass("visible");
 });
 
 $(document).click(function(event) {
   //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest(".js-open-modal5").length) {
-    $("body").find(".modal").removeClass("visible");
+  if (!$(event.target).closest(".modal5, .js-open-modal5").length) {
+    $("body").find(".modal5").removeClass("visible");
   }
 });
 
