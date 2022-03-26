2e37c415d52f1539b97bb916291508be9143dee9
kimh559 <kimh559@newschool.edu>
2022-03-26 19:52:39 -0400

    update

---
+++ lab-2022/assignment-6/bubble.js
@@ -0,0 +1,13 @@
+let bubbles = document.querySelectorAll('bubble');
+let container = document.querySelectorAll('#container');
+
+bubbles.forEach(function (bubble) {
+    bubble.style.left = (90 * Math.random()) + '%';
+    bubble.style.top = (100 * Math.random()) + '%';
+});
+
+container.addEventListener('click', function (event) {
+    if (event.target.classList.contains('bubble')) {
+        event.target.remove();
+    }
+});
