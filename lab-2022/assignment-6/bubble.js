3614ce8496ff803dcc47968d22c281ca22c7bb1f
kimh559 <kimh559@newschool.edu>
2022-03-27 11:27:35 -0400

    update

--- lab-2022/assignment-6/bubble.js
+++ lab-2022/assignment-6/bubble.js
@@ -1,6 +1,12 @@
 let bubbles = document.querySelectorAll('.bubble');
 let container = document.querySelector('#container');
 
+for (let i = 0; i < 50; i++) {
+    let bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    container.appendChild(bubble);
+}
+
 bubbles.forEach(function (bubble) {
     bubble.style.left = (90 * Math.random()) + '%';
     bubble.style.top = (100 * Math.random()) + '%';
